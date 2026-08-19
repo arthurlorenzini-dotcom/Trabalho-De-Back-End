040ef13feda5b89ebadba5003843c69076e05280
arthurlorenzini-dotcom <arthur.lorenzini@escola.pr.gov.br>
2026-08-19 15:24:40 +0000

    Trabalho Back-End

---
+++ Novo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Novo {
+
+    public static void main(String[] args) {
+        
+        System.out.println("{!--SISTEMA BANCARIO--!}");
+        System.out.println("{   [1]Depositar       }");
+        System.out.println("{   [2]Sacar           }");
+        System.out.println("{   [3]Extrato         }");
+        System.out.println("{   [4]Sair            }");
+        
+    }
+}
